eeaff135c4a8eca60f3570d4ef6a57084468f8b5
Gracjan <graku12@gmail.com>
2020-04-12 23:11:20 +0200

    Add ctors for Driver

--- Passenger.Core/Domain/Driver.cs
+++ Passenger.Core/Domain/Driver.cs
@@ -6,10 +6,34 @@ namespace Passenger.Core.Domain
     public class Driver
     {
         public Guid UserId { get; protected set; }
+        public string Username { get; set; }
         public Vehicle Vehicle { get; protected set; }
         public IEnumerable<Route> Routes { get; protected set; }
-        public IEnumerable<DailyRoute> DailyRoutes { get; protected set; }
         public DateTime UpdatedAt { get; protected set; }
+
+        protected Driver()
+        {   
+        }
+
+        public Driver(User user)
+        {
+            UserId = user.Id;
+            Username = user.Username;
+        }
+
+        public void SetVehicle(Vehicle vehicle)
+        {
+            Vehicle = vehicle;
+            UpdatedAt = UpdatedAt;
+        }
+
+        public void AddRoute(Route route)
+        { 
+        }
+
+        public void DeleteRoute(string name)
+        {
+        }
 
     }
 }
