497e33df1a46cee36e91cf5df942f0c89161b025
Gracjan <graku12@gmail.com>
2020-04-21 21:50:10 +0200

    Add converter and AddUserCommand

---
+++ src/Passenger.Infrastructure/Commands/User/AddUserCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace Passenger.Infrastructure.Commands.User
+{
+  public class AddUserCommand : ICommand
+  {
+    public string Username { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+  }
+}
---
+++ src/Passenger.Infrastructure/Converters/DriverDtoConverter.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Passenger.Core.Domain;
+using Passenger.Infrastructure.Converters.Interfaces;
+using Passenger.Infrastructure.DTO;
+
+namespace Passenger.Infrastructure.Converters
+{
+  public class DriverDtoConverter : IDriverDtoConverter
+  {
+    private readonly IMapper _mapper;
+
+    public DriverDtoConverter()
+    {
+      var configuration = new MapperConfiguration(cfg =>
+      {
+        cfg.CreateMap<Driver, DriverDto>();
+      });
+
+      configuration.AssertConfigurationIsValid();
+      _mapper = configuration.CreateMapper();
+    }
+
+    public DriverDto Convert(Driver driver)
+      => _mapper.Map<DriverDto>(driver);
+  }
+}
