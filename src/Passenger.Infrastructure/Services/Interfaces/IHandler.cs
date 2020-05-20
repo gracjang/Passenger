085440df9fb5dc94ea71f8c71acdf0a8b481e7e2
Gracjan <graku12@gmail.com>
2020-05-20 06:07:20 +0200

    Add Handler, HandlerTask and HandlerTaskRunner

---
+++ src/Passenger.Infrastructure/Services/Interfaces/IHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Passenger.Infrastructure.Services.Interfaces
+{
+  public interface IHandler
+  {  
+    IHandlerTask Run(Func<Task> run);
+
+    IHandlerTaskRunner Validate(Func<Task> validate);
+
+    Task ExecuteAllAsync();
+  }
+}
